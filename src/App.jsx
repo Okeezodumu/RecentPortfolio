08df8e62f062989dd333957c23e3a2cd5f3e43e2
unknown <Okeez Odumu>
2024-04-05 18:11:27 +0100

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,20 @@ const responsive = {
 
 const App = () => {
   React
-  return (
+  return ( 
     <div className='container m-auto'>
 
-      <section className='lg:flex-col  md:mx-20 float-left p-4 rounded-xl  bg-gray shadow-black shadow-xl m-10  animate-bounce ' >
+      <section className='lg:flex-col  md:mx-20 float-left p-4 rounded-full  bg-black shadow-black shadow-xl m-10  animate-bounce ' >
         <h2 className=' text-white font-bold lg: text-2xl md: text-xl text-center '> Okopi <font>Johnson</font> Odumu</h2>
         <h1 className=' text-white text-center lg:text-5xl md: text-xl text-center font-bold '><font>Soft</font>ware <font>Dev</font>eloper and <font> Eng</font>ineer</h1>
-        <p className='text-white text-center'>Nothing is impossible if you are willing to learn, inovate and build</p>
+        <p className='text-white text-center my-5'>My motto is to learn, inovate and build</p>
 
       </section>
       <div className='flex clear-both justify-center gap-5 m-2 ' >
         <a href="https://www.linkedin.com/in/odumu-okopi-370354245?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BKzgrLHKASrm5tJ%2FJlqU%2FMQ%3D%3D"><img className='w-10 ' src="/linkedin.svg" alt="" /></a>
         <a href="https://github.com/Okeezodumu"><img className='w-10 ' src="/github.svg" alt="" /></a>
         <a href="https://twitter.com/okeezdupi2"><img className='w-10 ' src="/x-social-media-logo-icon.svg" alt="" /></a>
-        <a href="https://drive.google.com/file/d/1lg-p571BR3nrWMQRmT-Q15e8EcUOYGHU/view?usp=drive_link"><button className='bg-black text-white shadow-xl shadow-gray-dark lg:p-5 md: p-2 rounded-xl  hover:bg-gray-dark'>Download my CV</button></a>
+        <a href="https://drive.google.com/file/d/1LH4NMzb_Mt2viYo00r-uAsV1E6v0dqbX/view?usp=drivesdk"><button className='bg-purple text-white shadow-xl shadow-gray-dark lg:p-5 md: p-2 rounded-xl  hover:bg-gray-dark'>Download my CV</button></a>
       </div>
 
       <section className='flex-col p-2  m-auto'>
@@ -64,7 +64,7 @@ const App = () => {
         </div>
         <div className='flex gap-10 justify-center   my-10 animate-pulse'>
           <img className='lg:h-24 md: h-12' src="/React logo.svg" alt="" />
-          <img className='lg:h-24 md: h-12' src="/Node js logo.png" alt="" />
+          <img className='lg:h-24 md: h-12' src="/microsoft-excel-icon.png" alt="" />
           <img className='lg:h-24 md: h-12' src="/mongodb logo.png" alt="" />
           <img className='lg:h-24 md: h-12' src="/SQl logo.svg" alt="" />
         </div>
@@ -79,22 +79,35 @@ const App = () => {
 
 
       <Carousel infinite={true} autoPlay={this !== "mobile" ? true : false} className='saturate-150 h-2xl w-full shadow-xl shadow-black  rounded-2xl' responsive={responsive}>
-        <div className='mx-5 shadow-xl shadow-purple'><img src="/SharedScreenshot.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-purple'><img src="/SharedScreenshot2.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-purple'><img src="/SharedScreenshot1.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-purple'><img src="/SharedScreenshot3.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-blue'><img src="/Bookme1.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-blue'><img src="/Bookme2.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-blue'><img src="/Bookme3.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-green'><img src="/Brill1.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-green'><img src="/Brill2.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-green'><img src="/Brill3.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-pink'><img src="/cruise1.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-pink'><img src="/cruise2.jpg" alt="" /></div>
-        <div className='mx-5 shadow-xl shadow-pink'><img src="/cruise3.jpg" alt="" /></div>
-      </Carousel>;
-    </div>
+        <div className='mx-5 '><img src="/SharedScreenshot.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/SharedScreenshot2.jpg" alt="" /></div>
+        <div className='mx-5'><img src="/SharedScreenshot1.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/SharedScreenshot3.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Bookme1.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Bookme2.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Bookme3.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Brill1.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Brill2.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/Brill3.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/cruise1.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/cruise2.jpg" alt="" /></div>
+        <div className='mx-5 '><img src="/cruise3.jpg" alt="" /></div>
+      </Carousel>
 
+      <div className='my-10 p-10 rounded-2xl shadow-black shadow-xl m-auto text-Poppins lg:text-xl text-purple bg-black sm:text-sm'>
+        1. So Much Cruise in This.
+        <p>A website that provides snippets for artiste and movie makers. These snippets can be purchased at request and have modifications carried out on them.Thw quality of the beats are authenthic and up to standard(.wav) with stems available also.</p>
+      </div>
+      <div className=' my-10 p-10 rounded-2xl shadow-black shadow-xl m-auto text-Poppins lg:text-xl text-white bg-black sm:text-sm'>
+        2. BookMe.
+        <p>A platform that provides an avenue for patients to book medical appoinments digitally.It provides services such as clinic and doctor name and time and choice of appointment.</p>
+      </div>
+      <div className=' my-10 p-10 rounded-2xl shadow-black shadow-xl m-auto text-Poppins lg:text-xl text-green bg-black sm:text-sm'>
+        3. Connorssiuer Realty.
+        <p>A website that provides the details of a realty company to connect and educate potentials constomers on the company and necessary information.</p>
+      </div>
+    </div>
+   
 
   )
 
